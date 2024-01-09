8604bb9387e76a9871535a2cf6e846db3d0a1999
playma <scott820914@gmail.com>
2024-01-09 17:23:33 +0800

    feat: alter name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://docs.payfect.finance.com',
+  url: 'https://docs.payfect.finance',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
